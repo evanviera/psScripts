b0ca303e2564193a27e44f96d295c59ef64adf4d
Evan Viera <evanviera@gmail.com>
2019-06-12 01:05:59 -0700

    Adding

---
+++ ConvertRgbToMask.jsx
@@ -0,0 +1,16 @@
+<javascriptresource>
+	<name>Convert RGB to Mask</name>
+	<about>
+			This will convert the RGB values of a given layer and convert it to
+			a layer mask. This only calls a function defined in vieraLibrary.jsx.
+			Evan Viera
+	</about>
+	<menu>filter</menu>
+	<category>Viera</category>
+	<type>automate</type>
+	<enableinfo>true</enableinfo>
+	</javascriptresource>
+
+#include "vieraLibrary.jsx"
+
+convertRGBToMask( );
