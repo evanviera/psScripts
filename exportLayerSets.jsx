0bfad1699c26a1e9e2eb39c1219a48a97c90dd7e
Evan Viera <evanviera@gmail.com>
2019-10-08 19:37:18 -0700

    Intial commit for this. Place holder for now

---
+++ exportLayerSets.jsx
@@ -0,0 +1,9 @@
+<javascriptresource>
+	<name>Export Top-level Layer Sets</name>
+	<about>
+			This will export top level
+      Layer-sets
+	</about>
+	<category>Viera</category>
+	<enableinfo>true</enableinfo>
+	</javascriptresource>
