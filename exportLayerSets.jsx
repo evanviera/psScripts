94e26989119bba6cda580cffdb164f823b8f89e6
Evan Viera <evanviera@gmail.com>
2019-10-09 23:52:35 -0700

    This is now working as it should

--- exportLayerSets.jsx
+++ exportLayerSets.jsx
@@ -10,43 +10,43 @@
 
 #include "vieraLibrary.jsx"
 
-var doc = activeDocument;
+app.displayDialogs = DialogModes.NO;
 
-for ( var i = 0; i < doc.layers.length; i++ )
+var doc = app.activeDocument;
+var docRef;
+var pngSaveOptions = new PNGSaveOptions( );
+		pngSaveOptions.compression = 9;
+
+var refLayer = new RegExp( /_REF_/gim );
+
+for ( var i = doc.layers.length - 1; i >= 0; i-- )
 {
   if ( doc.layers[ i ].typename == "LayerSet" )
 	{
-		doc.activeLayer = doc.layers[ i ];
-		setName = doc.layers[ i ].name;
-		layerMaskToSelection( );
+		if ( !doc.layers[ i ].name.match( refLayer ) )
+		{
+			doc.activeLayer = doc.layers[ i ];
+			layerMaskToSelection( );
 
-		var savePath = app.activeDocument.path + "/png/";
+			var docName = new String( doc.name );
+					docName = docName.slice( 0 , -4 );
 
-		app.displayDialogs = DialogModes.NO;
+			var grpName = new String( doc.layers[ i ].name );
+					grpName = grpName.slice( 3 );
 
-		var pngSaveOptions = new PNGSaveOptions();
-		pngSaveOptions.compression = 9;
+			var outputPath = new File( 	doc.path + "/png/" + docName +
+																	grpName + ".png" );
 
-		var hasSelection;
-		var docRef;
-		try {
-		    hasSelection = !!app.activeDocument.selection.bounds;
-		} catch (err) {
-		    hasSelection = false;
+			app.activeDocument.selection.copy( true );
+			var w = app.activeDocument.selection.bounds[ 2 ];
+			var h = app.activeDocument.selection.bounds[ 3 ];
+			docRef = app.documents.add( w, h );
+			docRef.paste( );
+			docRef.trim( );
+			docRef.trim( );
+
+			docRef.saveAs( outputPath, pngSaveOptions, true, Extension.LOWERCASE );
+			docRef.close( SaveOptions.DONOTSAVECHANGES );
 		}
-
-		if ( hasSelection ) {
-		    app.activeDocument.selection.copy( true );
-		    var w = app.activeDocument.selection.bounds[ 2 ];
-		    var h = app.activeDocument.selection.bounds[ 3 ];
-		    docRef = app.documents.add( w, h );
-		    docRef.paste( );
-				docRef.trim( );
-				docRef.trim( );
-		} else {
-		    docRef = app.activeDocument;
-		}
-		    docRef.saveAs( new File( savePath+doc.name+setName+".png" ), pngSaveOptions, !hasSelection, Extension.LOWERCASE);
-		    docRef.close(SaveOptions.DONOTSAVECHANGES);
 	}
 }
