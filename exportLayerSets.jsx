4d1d1ca7b21f26c447e72631d8c56b0b461667f8
Evan Viera <evanviera@gmail.com>
2019-10-09 19:09:35 -0700

    To work on at home

--- exportLayerSets.jsx
+++ exportLayerSets.jsx
@@ -14,6 +14,41 @@ for ( var i = 0; i < doc.layers.length; i++ )
 {
   if ( doc.layers[ i ].typename == "LayerSet" )
 	{
-		alert( doc.layers[ i ].name );	
+		alert( doc.layers[ i ].name );
 	}
 }
+
+
+/*
+
+app.displayDialogs = DialogModes.NO;
+
+var pngSaveOptions = new PNGSaveOptions();
+pngSaveOptions.compression = 9;
+
+var hasSelection;
+var docRef;
+try {
+    hasSelection = !!app.activeDocument.selection.bounds;
+} catch (err) {
+    hasSelection = false;
+}
+
+if (hasSelection) {
+    app.activeDocument.selection.copy(true);
+    var w = app.activeDocument.selection.bounds[2];
+    var h = app.activeDocument.selection.bounds[3];
+    docRef = app.documents.add(w, h);
+    docRef.paste();
+} else {
+    docRef = app.activeDocument;
+}
+var file = File.saveDialog("Export as PNG to...");
+if (file && ((file.exists && confirm("Overwrite " + file +"?")) || !file.exists)) {
+    docRef.saveAs(file, pngSaveOptions, !hasSelection, Extension.LOWERCASE);
+    if (hasSelection) {
+    docRef.close(SaveOptions.DONOTSAVECHANGES);
+    }
+}
+
+*/
