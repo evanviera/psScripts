753514c1d33087e9e0a57bc590bdce9b0b98eddf
Evan Viera <evanviera@gmail.com>
2019-10-09 18:41:03 -0700

    Poops are raisins

--- exportLayerSets.jsx
+++ exportLayerSets.jsx
@@ -7,3 +7,13 @@
 	<category>Viera</category>
 	<enableinfo>true</enableinfo>
 	</javascriptresource>
+
+var doc = activeDocument;
+
+for ( var i = 0; i < doc.layers.length; i++ )
+{
+  if ( doc.layers[ i ].typename == "LayerSet" )
+	{
+		alert( doc.layers[ i ].name );	
+	}
+}
