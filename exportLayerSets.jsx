50974dc18a01f3ff89e2ee23a97d08254a067f8d
Evan Viera <evanviera@gmail.com>
2019-10-10 08:14:24 -0700

    Gornoplat

--- exportLayerSets.jsx
+++ exportLayerSets.jsx
@@ -2,7 +2,7 @@
 	<name>Export Top-level Layer Sets</name>
 	<about>
 			This will export top level
-      Layer-sets
+      Layer-sets.
 	</about>
 	<category>Viera</category>
 	<enableinfo>true</enableinfo>
@@ -14,10 +14,19 @@ app.displayDialogs = DialogModes.NO;
 
 var doc = app.activeDocument;
 var docRef;
+
 var pngSaveOptions = new PNGSaveOptions( );
-		pngSaveOptions.compression = 9;
+		pngSaveOptions.compression = 2;
+
+var jpegSaveOptions = new JPEGSaveOptions( );
+		jpegSaveOptions.quality = 12;
+		jpegSaveOptions.embedColorProfile = true;
+		jpegSaveOptions.matte = MatteType.BLACK;
 
 var refLayer = new RegExp( /_REF_/gim );
+
+var docName = new String( doc.name );
+		docName = docName.slice( 0 , -4 );
 
 for ( var i = doc.layers.length - 1; i >= 0; i-- )
 {
@@ -26,27 +35,32 @@ for ( var i = doc.layers.length - 1; i >= 0; i-- )
 		if ( !doc.layers[ i ].name.match( refLayer ) )
 		{
 			doc.activeLayer = doc.layers[ i ];
-			layerMaskToSelection( );
+			if ( !hasLayerMask( ) )
+			{
+				alert( "LayerSet Requires Mask" );
+			}
+			else
+			{
+				layerMaskToSelection( );
 
-			var docName = new String( doc.name );
-					docName = docName.slice( 0 , -4 );
+				var grpName = new String( doc.layers[ i ].name );
+						grpName = grpName.slice( 3 );
 
-			var grpName = new String( doc.layers[ i ].name );
-					grpName = grpName.slice( 3 );
+				var outputPath = new File( 	doc.path + "/png/" + docName +
+																		grpName + ".png" );
 
-			var outputPath = new File( 	doc.path + "/png/" + docName +
-																	grpName + ".png" );
+				doc.selection.copy( true );
+				var w = doc.selection.bounds[ 2 ];
+				var h = doc.selection.bounds[ 3 ];
+				docRef = app.documents.add( w, h );
+				docRef.paste( );
+				docRef.trim( );
+				docRef.trim( );
 
-			app.activeDocument.selection.copy( true );
-			var w = app.activeDocument.selection.bounds[ 2 ];
-			var h = app.activeDocument.selection.bounds[ 3 ];
-			docRef = app.documents.add( w, h );
-			docRef.paste( );
-			docRef.trim( );
-			docRef.trim( );
-
-			docRef.saveAs( outputPath, pngSaveOptions, true, Extension.LOWERCASE );
-			docRef.close( SaveOptions.DONOTSAVECHANGES );
+				docRef.saveAs( outputPath, pngSaveOptions, true, Extension.LOWERCASE );
+				docRef.saveAs( outputPath, jpegSaveOptions, true, Extension.LOWERCASE );
+				docRef.close( SaveOptions.DONOTSAVECHANGES );
+			}
 		}
 	}
 }
