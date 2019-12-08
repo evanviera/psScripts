56a462728fb278e0c0b853e5af7a6dfc59874681
Evan Viera <evanviera@gmail.com>
2019-12-08 10:25:35 -0800

    New script

---
+++ toggleCurrentLayer.jsx
@@ -0,0 +1,11 @@
+<javascriptresource>
+	<name>Toggle Current Layer</name>
+	<about>
+			This will toggle the visibility of the
+			current layer.
+	</about>
+	<category>Viera</category>
+	<enableinfo>true</enableinfo>
+	</javascriptresource>
+
+activeDocument.activeLayer.visible = !activeDocument.activeLayer.visible;
